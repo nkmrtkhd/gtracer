860191f774094aeb67a65ebb373bfc13ca93ed12
Takahide NAKAMURA <nkmrtkhd@gmail.com>
2011-02-09 17:15:18 +0900

    GTracer project starts!!

---
+++ GTracer.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.io.*;
+import java.util.*;
+
+
+public class GTracer{
+  public static void main(String[] args){
+    new GTracer();
+  }//end of main
+
+  //constructor
+  public GTracer(){
+  }
+
+}
