595e3deec654fc1e8f1c258f1c322c0cbf4c0747
Takahide NAKAMURA <nkmrtkhd@gmail.com>
2011-02-14 00:07:45 +0900

    refactoring and add comment

--- GTracer.java
+++ GTracer.java
@@ -6,11 +6,11 @@ import javax.imageio.*;
 import javax.swing.*;
 import javax.swing.event.*;
 
+//hand made library
 import filter.*;
 
 
 public class GTracer extends JFrame implements ActionListener{
-
 
   //main function
   public static void main(String[] args) {
@@ -24,8 +24,9 @@ public class GTracer extends JFrame implements ActionListener{
 
   //constructor
   public GTracer(String filename){
-    setTitle("GTracer");
+    super("GTracer");//new JFrame
 
+    //load image file
     try{
       File imgfile = new File(filename);
       originalImg = ImageIO.read(imgfile);
@@ -33,11 +34,15 @@ public class GTracer extends JFrame implements ActionListener{
     }
     tracedImg=null;
 
+    //window size
     setBounds( 0, 0, 850, 500);
+    //create panel and add to this(JFrame)
     add(makePanel());
+    //how to action, when close
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     setVisible(true);
 
+    //tracer
     tracer=new Tracer();
   }
 
@@ -51,17 +56,21 @@ public class GTracer extends JFrame implements ActionListener{
 
 
   private JButton doTraceButton;
-  MyCanvas myCanv;
+  private MyCanvas myCanv;
   private JPanel makePanel(){
+
+    //button
     doTraceButton=new JButton("trace");
     doTraceButton.addActionListener( this );
     doTraceButton.setFocusable(false);
 
+    //canvas
     myCanv=new MyCanvas();
     myCanv.setBackground(Color.white);
 
+    //panel
     JPanel jp=new JPanel();
-    //layout
+    //set layout
     SpringLayout layout = new SpringLayout();
     jp.setLayout( layout );
     layout.putConstraint( SpringLayout.SOUTH, doTraceButton, -5,SpringLayout.SOUTH, jp );
@@ -72,13 +81,14 @@ public class GTracer extends JFrame implements ActionListener{
     layout.putConstraint( SpringLayout.WEST, myCanv, 0,SpringLayout.WEST, jp );
     layout.putConstraint( SpringLayout.EAST, myCanv, 0,SpringLayout.EAST, jp );
 
+    //add to jpanel
     jp.add(myCanv);
     jp.add(doTraceButton);
     return jp;
   }
 
-  ///
-  class MyCanvas extends Canvas{
+  ///private class
+  private class MyCanvas extends Canvas{
     public void paint(Graphics g){
       g.drawImage(originalImg,0,20,this);
 
--- filter/Tracer.java
+++ filter/Tracer.java
@@ -11,12 +11,28 @@ public class Tracer{
   //constructor
   public Tracer(){
   }
-  public BufferedImage startTrace(BufferedImage original){
-    BufferedImage traced=null;
 
+  public BufferedImage startTrace(BufferedImage orgBimg){
+    //copy original to copyBimg
+    BufferedImage copyBimg = new BufferedImage(orgBimg.getWidth(),
+                                               orgBimg.getHeight(),
+                                               orgBimg.getType());
+    Graphics2D g2 = copyBimg.createGraphics();
+    g2.drawImage(orgBimg, 0, 0, null);
+    //convert to gray scale
+    for (int y = 0; y < orgBimg.getHeight();y++){
+      for (int x = 0; x < orgBimg.getWidth();x++){
+        int rgb = orgBimg.getRGB(x, y);
+        int a = (rgb>> 24) & 0xff;
+        int r = (rgb >> 16) & 0xff;
+        int g = (rgb >> 8) & 0xff;
+        int b = rgb & 0xff;
+        int m = (2*r + 4*g + b) / 7;
+        copyBimg.setRGB(x, y, new Color(m, m, m, a).getRGB());
+      }
+    }
 
-
-    return original;
+    return copyBimg;
   }
 
 }
